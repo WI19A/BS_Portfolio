6506955b7802c80caded0ce11af2a5c47c4fd976
SiG268 <s.r.geissler@gmail.com>
2020-04-20 21:06:47 +0200

    reworked race condition on prod_cons

--- Producer_Consumer/race_condition.py
+++ Producer_Consumer/race_condition.py
@@ -1,42 +1,57 @@
-from threading import Thread
+import threading
 import time
+import random
 
-cedric = 0
-#Implementiert eine Methode
-def Producer(x):
-    #System Out
-    #print("Start Thread%s" % x)
-    for _ in range(100000):
-        inc()
-    #print("Stop Thread%s" % x)
 
-def Consumer(x):
-    global cedric
-    print(cedric)
+index = 0
+q = []
+
+class Producer(threading.Thread): #Producer erbt von Thread
+    def __init__(self, iD, name):   
+        threading.Thread.__init__(self) #super Constructer
+        self.product = ['Haselnuss', 'Schnitzelbroetchen', 'Senf']
+        self.iD = iD
+        self.name = name
+
+    def run(self):
+        global q
+        global index
+        while index < 10:
+            #Random Produktionszeit(0-3 sekunden)
+            time.sleep(1) #Kein Random Sleep, um raceCondition zu zeigen(Threads muessen gleichzeitig was veraendern)
+            f = self.product[random.randrange(len(self.product))]
+            q.insert(index, f)
+            print("Thread{1} Added: {0} on {2}".format(f, self.iD, index))
+            index+=1
+            
+
+
+class Consumer(threading.Thread):
+    def __init__(self, iD, name):
+        threading.Thread.__init__(self)
+        self.iD = iD
+        self.name = name
+
+    def run(self):
+        global q
+        #while True:
+            #if (len(q)>9):
+            #Random Cosumzeit(0-2 Sekunden)
+
+            
+
+
+if __name__ == '__main__':
     
-def inc():
-    global cedric
-    cedric += 1
+    #Threadstart
+    p1 = Producer(1,"Prod1")
+    p2 = Producer(2,"Prod2")
 
+    #c1 = Consumer(1,"Cons1")
 
-
-#Methodenaufruf
-def start():
-    global cedric
-    cedric = 0
-    t1=Thread(target=Producer, args=("Producer1", ))
-    t2=Thread(target=Producer, args=("Producer2", ))
-
-    t1.start()
-    t2.start()
-
-    t1.join()
-    t2.join()
-    t3=Thread(target=Consumer, args=("Consumer", ))
-    t3.start()
-
-    
-
-for i in range(10):
-    start()
-    #print("Iteration",i, cedric)
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+    print(q)
+    #c1.start()
