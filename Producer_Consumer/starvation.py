776326b82c76cf88f3d823a69f0444a0cfe851d0
SiG268 <s.r.geissler@gmail.com>
2020-04-20 15:36:19 +0200

    überarbeitung starvation cons_prod

--- Producer_Consumer/starvation.py
+++ Producer_Consumer/starvation.py
@@ -1,35 +1,47 @@
 from threading import Thread
 import time
+import random
+import queue
 
-cedric = 0
-markus = 0
-#Implementiert eine Methode
-def Producer(x):
-    #System Out
-    print("Start Thread%s" % x)
-    cedric1 = 0
-    for _ in range(10):
-        global cedric
-        time.sleep(1)
-        cedric1 = cedric1 + 1
-        cedric = cedric1
-        print("prod",cedric)
-    print("Stop Thread%s" % x)
-    
+class Producer:
+    def __init__(self):
+        self.product = ['Haselnuss', 'Schnitzelbroetchen', 'Senf']
 
-def Consumer(x):
-    #System Out
-    print("Start Thread%s" % x)
-    for _ in range(10):
-        global cedric
-        global markus
-        while cedric == 0:
-            time.sleep(0.5)
-            print("Ich verhungere")
-        markus = markus+ cedric
-        cedric = 0
-        print("cons",markus)
-    print("Stop Thread%s" % x)
+    def run(self):
+        global q
+        while True:
+            #Random Produktionszeit(0-10 sekunden)
+            time.sleep(random.random()*3)
+            f = self.product[random.randrange(len(self.product))]
+            q.put(f)
+            print("Added: {}".format(f))
+
+
+class Consumer:
+    def __init__(self):
+        self.next = 0
+
+    def run(self):
+        global q
+        while True:
+            #Random Cosumzeit(0-2 Sekunden)
+            time.sleep(random.random()*2)
+            print('Queue size: {}'.format(q.qsize()))
+            if (not q.empty()):
+                f = q.get()
+                print("Remove: {}".format(f))
+            else:
+                print('Consumer is waiting for product!')
+
+
+if __name__ == '__main__':
+    q = queue.Queue(10)
+    p = Producer()
+    c = Consumer()
+    pt = Thread(target=p.run, args = ())
+    ct = Thread(target=c.run, args = ())
+    pt.start()
+    ct.start()
 
 
 
---
+++ test2.py
@@ -0,0 +1,4 @@
+q = ['Haselnuss', 'Schnitzelbroetchen', 'Senf']
+print(len(q))
+if len(q)!=10:
+    print("Erfolg")
